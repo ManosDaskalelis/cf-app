523ada545c5ff91d2d618234f18248ee17872f9c
Manos Daskalelis <Manolisdaskalelis@gmai.com>
2024-03-26 19:02:48 +0200

    docker file

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+// const port = 3000;
+const mongoose = require('mongoose');
 
-app.use(express.json())
+require('dotenv').config();
 
-const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./swagger')
+app.use(express.json());
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger');
 
 mongoose.connect(process.env.MONGODB_URI)
-    .then(
-        () => { console.log('Connection established') },
-        err => { console.log('failed to connect', err); }
-    )
+  .then(
+    () => { console.log("Connection to mongodb established")},
+    err => { console.log("Failed to connect to mongodb", err)}
+  );
 
-const user = require('./routes/user.routes')
+const cors = require('cors');
+app.use(cors({
+  origin: '*'
+  // origin: ['http://localhost:8000/', 'http://www.aueb.gr']
+}))
+
+const user = require('./routes/user.route');
 const userProduct = require('./routes/user.products.routes')
 
+app.use('/', express.static('files'));
 app.use('/api/users', user)
 app.use('/api/user-products', userProduct)
 
 app.use('/api-docs', 
-swaggerUi.serve,
-swaggerUi.setup(swaggerDocument.options)
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument.options)
 )
 
-app.listen(port, () => {
-    console.log("server is up");
-})
+// app.listen(port, ()=>{
+//   console.log("Server is up");
+// })
 
+module.exports = app;
--- models/product.model.js
+++ models/product.model.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema
 
-let productsSchema = new Schema({
-    product: {type: String, required: true},
-    cost:{type: Number, required: true},
-    description: {type: String, required: true},
-    quantity: {type: Number, required: true}
+let productSchema = new Schema({
+  product: { type: String, required: true },
+  cost: { type: Number, required: true},
+  description: { type: String, required: true},
+  quantity: { type: Number, required: true}
 }, {
-    collection: 'products',
-    timestamps: true
-})
+  collection: 'products',
+  timestamps: true
+});
 
-module.exports = mongoose.model('Products', productsSchema)
+module.exports = mongoose.model('Product', productSchema);
